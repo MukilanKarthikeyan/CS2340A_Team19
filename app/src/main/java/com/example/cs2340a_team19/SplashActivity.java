70daee49157c47db09cb3593e14c8f61da081cb9
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-02-24 23:38:48 -0500

    app would not start as the SplashActivity.java could not locate MainActivity.java. fixed issue with import statement. App now runs but crashes after splash screen

--- app/src/main/java/com/example/cs2340a_team19/SplashActivity.java
+++ app/src/main/java/com/example/cs2340a_team19/SplashActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.cs2340a_team19.views.MainActivity;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
