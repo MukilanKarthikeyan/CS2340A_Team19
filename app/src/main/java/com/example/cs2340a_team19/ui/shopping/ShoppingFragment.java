4e455df9cd313d8fd261d1d040780292ce83059f
RusDoomer <a.i.iche.609@gmail.com>
2024-02-25 22:54:14 -0500

    checkstyle

--- app/src/main/java/com/example/cs2340a_team19/ui/shopping/ShoppingFragment.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/shopping/ShoppingFragment.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.cs2340a_team19.databinding.FragmentDashboardBinding;
 import com.example.cs2340a_team19.databinding.FragmentShoppingBinding;
 
 public class ShoppingFragment extends Fragment {
