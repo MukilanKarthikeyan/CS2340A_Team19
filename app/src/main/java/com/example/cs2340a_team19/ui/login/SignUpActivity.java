0b5ce900b0248ffaa7f41ba78110bd148ad77ca2
RusDoomer <a.i.iche.609@gmail.com>
2024-02-25 20:07:03 -0500

    preliminary implementation of signup screen

--- app/src/main/java/com/example/cs2340a_team19/ui/login/SignUpActivity.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/login/SignUpActivity.java
@@ -29,17 +29,17 @@ import com.example.cs2340a_team19.MainActivity;
 import com.example.cs2340a_team19.R;
 import com.example.cs2340a_team19.ui.login.LoginViewModel;
 import com.example.cs2340a_team19.ui.login.LoginViewModelFactory;
-import com.example.cs2340a_team19.databinding.ActivityLoginBinding;
+import com.example.cs2340a_team19.databinding.UserSignUpBinding;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
-    private ActivityLoginBinding binding;
+    private UserSignUpBinding binding;
     private FirebaseAuth mAuth;
     private FirebaseUser currUser;
 
@@ -49,7 +49,7 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = UserSignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 //        loginViewModel = new LoginViewModel();
@@ -58,68 +58,9 @@ public class LoginActivity extends AppCompatActivity {
         final EditText passwordEditText = binding.password;
         final Button loginButton = binding.login;
         final ProgressBar loadingProgressBar = binding.loading;
-        final Button skipLoginTESTButton = binding.skipLoginTESTButton;
 
         loginButton.setEnabled(true);
-        final Button newSigninButton = binding.newSignIn;
 
-//        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-//            @Override
-//            public void onChanged(@Nullable LoginFormState loginFormState) {
-//                if (loginFormState == null) {
-//                    return;
-//                }
-//                loginButton.setEnabled(loginFormState.isDataValid());
-//                if (loginFormState.getUsernameError() != null) {
-//                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-//                }
-//                if (loginFormState.getPasswordError() != null) {
-//                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-//                }
-//            }
-//        });
-//
-//        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-//            @Override
-//            public void onChanged(@Nullable LoginResult loginResult) {
-//                if (loginResult == null) {
-//                    return;
-//                }
-//                loadingProgressBar.setVisibility(View.GONE);
-//                if (loginResult.getError() != null) {
-//                    showLoginFailed(loginResult.getError());
-//                }
-//                if (loginResult.getSuccess() != null) {
-//                    updateUiWithUser(loginResult.getSuccess());
-//                }
-//                setResult(Activity.RESULT_OK);
-//
-//                //Complete and destroy login activity once successful
-//                finish();
-//            }
-//        });
-
-//        TextWatcher afterTextChangedListener = new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//                // ignore
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-////                if (LoginViewModel.isUsernameValid(s)) {
-////                    loginButton.setEnabled(true);
-////                }
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-//                        passwordEditText.getText().toString());
-//            }
-//        };
-//        usernameEditText.addTextChangedListener(afterTextChangedListener);
-//        passwordEditText.addTextChangedListener(afterTextChangedListener);
         passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
             @Override
@@ -132,22 +73,14 @@ public class LoginActivity extends AppCompatActivity {
                 return false;
             }
         });
-        //TEST only: skips login step until login screen logic is implemented
 
-        skipLoginTESTButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadingProgressBar.setVisibility(View.VISIBLE);
 //                loginViewModel.login(usernameEditText.getText().toString(),
 //                        passwordEditText.getText().toString());
-                mAuth.signInWithEmailAndPassword(usernameEditText.getText().toString(), passwordEditText.getText().toString())
+                mAuth.createUserWithEmailAndPassword(usernameEditText.getText().toString(), passwordEditText.getText().toString())
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -156,16 +89,10 @@ public class LoginActivity extends AppCompatActivity {
                                     currUser = mAuth.getCurrentUser();
                                     updateUiWithUser();
                                 } else {
-                                    showLoginFailed("Firebase Authentication Failed");
+                                    showLoginFailed("Firebase User Creation / Authentication Failed");
                                 }
                             }
                         });
-            }
-        });
-        newSigninButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.user_sign_up);
             }
         });
     }
@@ -183,7 +110,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void updateUiWithUser() {
         String welcome = getString(R.string.welcome) + this.currUser.getDisplayName();
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
         startActivity(intent);
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
