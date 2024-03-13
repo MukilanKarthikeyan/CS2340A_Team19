198ca3b94b781670765c62f698767278d80d8ddf
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-03-12 20:58:07 -0400

    IT WORKS!!!!
    any chart now shows up! need to resize and updated stuff

--- app/src/main/java/com/example/cs2340a_team19/ui/meals/MealsFragment.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/meals/MealsFragment.java
@@ -100,12 +100,127 @@ public class MealsFragment extends Fragment {
         bar100.stroke("1 #e5e4e4");
         bar100.zIndex(4d);
 
+        circularGauge.label(1d)
+                .text("Guaifenesin, <span style=\"\">34%</span>")
+                .useHtml(true)
+                .hAlign(HAlign.CENTER)
+                .vAlign(VAlign.MIDDLE);
+        circularGauge.label(1d)
+                .anchor(Anchor.RIGHT_CENTER)
+                .padding(0d, 10d, 0d, 0d)
+                .height(17d / 2d + "%")
+                .offsetY(80d + "%")
+                .offsetX(0d);
+        Bar bar1 = circularGauge.bar(1d);
+        bar1.dataIndex(1d);
+        bar1.radius(80d);
+        bar1.width(17d);
+        bar1.fill(new SolidFill("#1976d2", 1d));
+        bar1.stroke(null);
+        bar1.zIndex(5d);
+        Bar bar101 = circularGauge.bar(101d);
+        bar101.dataIndex(5d);
+        bar101.radius(80d);
+        bar101.width(17d);
+        bar101.fill(new SolidFill("#F5F4F4", 1d));
+        bar101.stroke("1 #e5e4e4");
+        bar101.zIndex(4d);
+
+        circularGauge.label(2d)
+                .text("Salicylic Acid, <span style=\"\">67%</span>")
+                .useHtml(true)
+                .hAlign(HAlign.CENTER)
+                .vAlign(VAlign.MIDDLE);
+        circularGauge.label(2d)
+                .anchor(Anchor.RIGHT_CENTER)
+                .padding(0d, 10d, 0d, 0d)
+                .height(17d / 2d + "%")
+                .offsetY(60d + "%")
+                .offsetX(0d);
+        Bar bar2 = circularGauge.bar(2d);
+        bar2.dataIndex(2d);
+        bar2.radius(60d);
+        bar2.width(17d);
+        bar2.fill(new SolidFill("#ef6c00", 1d));
+        bar2.stroke(null);
+        bar2.zIndex(5d);
+        Bar bar102 = circularGauge.bar(102d);
+        bar102.dataIndex(5d);
+        bar102.radius(60d);
+        bar102.width(17d);
+        bar102.fill(new SolidFill("#F5F4F4", 1d));
+        bar102.stroke("1 #e5e4e4");
+        bar102.zIndex(4d);
+
+        circularGauge.label(3d)
+                .text("Fluoride, <span style=\"\">93%</span>")
+                .useHtml(true)
+                .hAlign(HAlign.CENTER)
+                .vAlign(VAlign.MIDDLE);
+        circularGauge.label(3d)
+                .anchor(Anchor.RIGHT_CENTER)
+                .padding(0d, 10d, 0d, 0d)
+                .height(17d / 2d + "%")
+                .offsetY(40d + "%")
+                .offsetX(0d);
+        Bar bar3 = circularGauge.bar(3d);
+        bar3.dataIndex(3d);
+        bar3.radius(40d);
+        bar3.width(17d);
+        bar3.fill(new SolidFill("#ffd54f", 1d));
+        bar3.stroke(null);
+        bar3.zIndex(5d);
+        Bar bar103 = circularGauge.bar(103d);
+        bar103.dataIndex(5d);
+        bar103.radius(40d);
+        bar103.width(17d);
+        bar103.fill(new SolidFill("#F5F4F4", 1d));
+        bar103.stroke("1 #e5e4e4");
+        bar103.zIndex(4d);
+
+        circularGauge.label(4d)
+                .text("Zinc Oxide, <span style=\"\">56%</span>")
+                .useHtml(true)
+                .hAlign(HAlign.CENTER)
+                .vAlign(VAlign.MIDDLE);
+        circularGauge.label(4d)
+                .anchor(Anchor.RIGHT_CENTER)
+                .padding(0d, 10d, 0d, 0d)
+                .height(17d / 2d + "%")
+                .offsetY(20d + "%")
+                .offsetX(0d);
+        Bar bar4 = circularGauge.bar(4d);
+        bar4.dataIndex(4d);
+        bar4.radius(20d);
+        bar4.width(17d);
+        bar4.fill(new SolidFill("#455a64", 1d));
+        bar4.stroke(null);
+        bar4.zIndex(5d);
+        Bar bar104 = circularGauge.bar(104d);
+        bar104.dataIndex(5d);
+        bar104.radius(20d);
+        bar104.width(17d);
+        bar104.fill(new SolidFill("#F5F4F4", 1d));
+        bar104.stroke("1 #e5e4e4");
+        bar104.zIndex(4d);
+
+        circularGauge.margin(50d, 50d, 50d, 50d);
+
+        circularGauge.title().enabled(true);
+        circularGauge.title().hAlign(HAlign.CENTER);
+        circularGauge.title()
+                .padding(0d, 0d, 0d, 0d)
+                .margin(0d, 0d, 20d, 0d);
 
         anyChartView.setChart(circularGauge);
     }
 
     @Override
     public void onViewCreated(View view, @NonNull Bundle savedInstanceState) {
+        // or  (ImageView) view.findViewById(R.id.foo);
+        //AnyChartView anyChartView = getView().findViewById(R.id.anychart_viz1_temp);
+        //anyChartView.setProgressBar(getView().findViewById(R.id.progress_bar));
+
        createAnyChart(view);
     }
 
