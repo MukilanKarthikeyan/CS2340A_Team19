b1d754245da14518f711c0ded090938500db59ee
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-03-12 21:44:22 -0400

    got title centered, must enlarge and make bar thicker

--- app/src/main/java/com/example/cs2340a_team19/ui/meals/MealsFragment.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/meals/MealsFragment.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -26,6 +24,17 @@ import com.anychart.graphics.vector.text.VAlign;
 import com.example.cs2340a_team19.R;
 import com.example.cs2340a_team19.databinding.FragmentMealsBinding;
 
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MealsFragment extends Fragment {
 
     private FragmentMealsBinding binding;
@@ -39,16 +48,18 @@ public class MealsFragment extends Fragment {
         binding = FragmentMealsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textMeals;
-        final ProgressBar progressView = binding.progressBar;
-
-        mealsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        mealsViewModel.getCalories().observe(getViewLifecycleOwner(), progressView::setProgress);
         //createAnyChart(root);
         return root;
     }
 
+    @Override
+    public void onViewCreated(View view, @NonNull Bundle savedInstanceState) {
+
+        createAnyChart(view);
+    }
+
     private void createAnyChart(View root){
+
         AnyChartView anyChartView = root.findViewById(R.id.anychart_viz1_temp);
         anyChartView.setProgressBar(getView().findViewById(R.id.anychart_progress_bar));
 
@@ -59,11 +70,11 @@ public class MealsFragment extends Fragment {
                 .padding(0d, 0d, 0d, 0d)
                 .margin(100d, 100d, 100d, 100d);
         circularGauge.startAngle(0d);
-        circularGauge.sweepAngle(270d);
+        circularGauge.sweepAngle(360d);
 
         Circular xAxis = circularGauge.axis(0)
                 .radius(100d)
-                .width(1d)
+                .width(4d)
                 .fill((Fill) null);
         xAxis.scale()
                 .minimum(0d)
@@ -75,153 +86,39 @@ public class MealsFragment extends Fragment {
         xAxis.minorTicks().enabled(false);
 
         circularGauge.label(0d)
-                .text("Temazepam, <span style=\"\">32%</span>")
+                .text("Calories, <span style=\"\">32%</span>")
                 .useHtml(true)
                 .hAlign(HAlign.CENTER)
                 .vAlign(VAlign.MIDDLE);
         circularGauge.label(0d)
-                .anchor(Anchor.RIGHT_CENTER)
+                .anchor(Anchor.CENTER)
                 .padding(0d, 10d, 0d, 0d)
-                .height(17d / 2d + "%")
-                .offsetY(100d + "%")
                 .offsetX(0d);
         Bar bar0 = circularGauge.bar(0d);
         bar0.dataIndex(0d);
-        bar0.radius(100d);
+        bar0.radius(150d);
         bar0.width(17d);
+
         bar0.fill(new SolidFill("#64b5f6", 1d));
         bar0.stroke(null);
         bar0.zIndex(5d);
         Bar bar100 = circularGauge.bar(100d);
         bar100.dataIndex(5d);
-        bar100.radius(100d);
+        bar100.radius(150d);
         bar100.width(17d);
         bar100.fill(new SolidFill("#F5F4F4", 1d));
         bar100.stroke("1 #e5e4e4");
         bar100.zIndex(4d);
 
-        circularGauge.label(1d)
-                .text("Guaifenesin, <span style=\"\">34%</span>")
-                .useHtml(true)
-                .hAlign(HAlign.CENTER)
-                .vAlign(VAlign.MIDDLE);
-        circularGauge.label(1d)
-                .anchor(Anchor.RIGHT_CENTER)
-                .padding(0d, 10d, 0d, 0d)
-                .height(17d / 2d + "%")
-                .offsetY(80d + "%")
-                .offsetX(0d);
-        Bar bar1 = circularGauge.bar(1d);
-        bar1.dataIndex(1d);
-        bar1.radius(80d);
-        bar1.width(17d);
-        bar1.fill(new SolidFill("#1976d2", 1d));
-        bar1.stroke(null);
-        bar1.zIndex(5d);
-        Bar bar101 = circularGauge.bar(101d);
-        bar101.dataIndex(5d);
-        bar101.radius(80d);
-        bar101.width(17d);
-        bar101.fill(new SolidFill("#F5F4F4", 1d));
-        bar101.stroke("1 #e5e4e4");
-        bar101.zIndex(4d);
-
-        circularGauge.label(2d)
-                .text("Salicylic Acid, <span style=\"\">67%</span>")
-                .useHtml(true)
-                .hAlign(HAlign.CENTER)
-                .vAlign(VAlign.MIDDLE);
-        circularGauge.label(2d)
-                .anchor(Anchor.RIGHT_CENTER)
-                .padding(0d, 10d, 0d, 0d)
-                .height(17d / 2d + "%")
-                .offsetY(60d + "%")
-                .offsetX(0d);
-        Bar bar2 = circularGauge.bar(2d);
-        bar2.dataIndex(2d);
-        bar2.radius(60d);
-        bar2.width(17d);
-        bar2.fill(new SolidFill("#ef6c00", 1d));
-        bar2.stroke(null);
-        bar2.zIndex(5d);
-        Bar bar102 = circularGauge.bar(102d);
-        bar102.dataIndex(5d);
-        bar102.radius(60d);
-        bar102.width(17d);
-        bar102.fill(new SolidFill("#F5F4F4", 1d));
-        bar102.stroke("1 #e5e4e4");
-        bar102.zIndex(4d);
-
-        circularGauge.label(3d)
-                .text("Fluoride, <span style=\"\">93%</span>")
-                .useHtml(true)
-                .hAlign(HAlign.CENTER)
-                .vAlign(VAlign.MIDDLE);
-        circularGauge.label(3d)
-                .anchor(Anchor.RIGHT_CENTER)
-                .padding(0d, 10d, 0d, 0d)
-                .height(17d / 2d + "%")
-                .offsetY(40d + "%")
-                .offsetX(0d);
-        Bar bar3 = circularGauge.bar(3d);
-        bar3.dataIndex(3d);
-        bar3.radius(40d);
-        bar3.width(17d);
-        bar3.fill(new SolidFill("#ffd54f", 1d));
-        bar3.stroke(null);
-        bar3.zIndex(5d);
-        Bar bar103 = circularGauge.bar(103d);
-        bar103.dataIndex(5d);
-        bar103.radius(40d);
-        bar103.width(17d);
-        bar103.fill(new SolidFill("#F5F4F4", 1d));
-        bar103.stroke("1 #e5e4e4");
-        bar103.zIndex(4d);
-
-        circularGauge.label(4d)
-                .text("Zinc Oxide, <span style=\"\">56%</span>")
-                .useHtml(true)
-                .hAlign(HAlign.CENTER)
-                .vAlign(VAlign.MIDDLE);
-        circularGauge.label(4d)
-                .anchor(Anchor.RIGHT_CENTER)
-                .padding(0d, 10d, 0d, 0d)
-                .height(17d / 2d + "%")
-                .offsetY(20d + "%")
-                .offsetX(0d);
-        Bar bar4 = circularGauge.bar(4d);
-        bar4.dataIndex(4d);
-        bar4.radius(20d);
-        bar4.width(17d);
-        bar4.fill(new SolidFill("#455a64", 1d));
-        bar4.stroke(null);
-        bar4.zIndex(5d);
-        Bar bar104 = circularGauge.bar(104d);
-        bar104.dataIndex(5d);
-        bar104.radius(20d);
-        bar104.width(17d);
-        bar104.fill(new SolidFill("#F5F4F4", 1d));
-        bar104.stroke("1 #e5e4e4");
-        bar104.zIndex(4d);
-
         circularGauge.margin(50d, 50d, 50d, 50d);
 
-        circularGauge.title().enabled(true);
+        circularGauge.title().enabled(false);
         circularGauge.title().hAlign(HAlign.CENTER);
         circularGauge.title()
                 .padding(0d, 0d, 0d, 0d)
                 .margin(0d, 0d, 20d, 0d);
 
         anyChartView.setChart(circularGauge);
-    }
-
-    @Override
-    public void onViewCreated(View view, @NonNull Bundle savedInstanceState) {
-        // or  (ImageView) view.findViewById(R.id.foo);
-        //AnyChartView anyChartView = getView().findViewById(R.id.anychart_viz1_temp);
-        //anyChartView.setProgressBar(getView().findViewById(R.id.progress_bar));
-
-       createAnyChart(view);
     }
 
     @Override
