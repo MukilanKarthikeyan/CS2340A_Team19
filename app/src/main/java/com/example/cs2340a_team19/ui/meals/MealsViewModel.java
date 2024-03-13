42dddeac678d23a73170e9605179466fa00e6b2c
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-03-12 23:29:22 -0400

    UI meal input screen parity fixing

--- app/src/main/java/com/example/cs2340a_team19/ui/meals/MealsViewModel.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/meals/MealsViewModel.java
@@ -1,5 +1,7 @@
 package com.example.cs2340a_team19.ui.meals;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
--- app/src/main/java/com/example/cs2340a_team19/ui/personalInfo/PersonalInformationFragment.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/personalInfo/PersonalInformationFragment.java
@@ -11,12 +11,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.cs2340a_team19.R;
+import com.example.cs2340a_team19.databinding.FragmentPersonalInformationBinding;
 
 public class PersonalInformationFragment extends Fragment {
 
     private PersonalInformationViewModel mViewModel;
+    private FragmentPersonalInformationBinding binding;
 
     public static PersonalInformationFragment newInstance() {
         return new PersonalInformationFragment();
@@ -27,6 +30,7 @@ public class PersonalInformationFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_personal_information, container, false);
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
