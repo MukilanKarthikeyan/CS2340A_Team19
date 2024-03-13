5dd6f12936cb05c63f5422d144b56c711c36953b
sidchill <siddharth.chilluvuri@gmail.com>
2024-03-12 22:06:38 -0400

    Created LoginViewModel JUnit Test, that tests all the possible invalid and valid password and email sequences

--- app/src/androidTest/java/com/example/cs2340a_team19/LoginViewModelTest.java
+++ app/src/androidTest/java/com/example/cs2340a_team19/LoginViewModelTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 
 import com.example.cs2340a_team19.ui.login.LoginViewModel;
 import java.util.Random;
-
+//JUnit Test for the LoginViewModel
 public class LoginViewModelTest {
 
     @Test
