12be6e9d0ae93f335c6bc1b32f43cbcf1a368c6f
alexichetov <alexichetov@gmail.com>
2024-03-12 21:18:07 -0400

    add recommendation engine

--- app/src/main/java/com/example/cs2340a_team19/models/Recomendation.java
+++ app/src/main/java/com/example/cs2340a_team19/models/Recomendation.java
@@ -1,4 +1,19 @@
 package com.example.cs2340a_team19.models;
 
 public class Recomendation {
+    private int calorie_goal;
+
+    public Recomendation() {
+        this.calorie_goal = 2000;
+    }
+
+    public Recomendation(int height, int weight, boolean gender) {
+        if(gender) {
+            //male
+            this.calorie_goal = (int) (10 * weight + (6.25 * (double)height) / 100 - 95);
+        } else {
+            //female
+            this.calorie_goal = (int) (10 * weight + (6.25 * (double)height) / 100 - 261);
+        }
+    }
 }
