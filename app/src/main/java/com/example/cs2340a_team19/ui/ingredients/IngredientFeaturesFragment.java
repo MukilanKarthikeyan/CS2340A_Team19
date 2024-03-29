ac9bbc34309efd64b0265916184f67eca24a3756
sidchill <siddharth.chilluvuri@gmail.com>
2024-03-29 19:35:08 -0400

    Created LoginViewModel JUnit Test, that tests all the possible invalid and valid password and email sequences

---
+++ app/src/main/java/com/example/cs2340a_team19/ui/ingredients/IngredientFeaturesFragment.java
@@ -0,0 +1,54 @@
+package com.example.cs2340a_team19.ui.ingredients;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.example.cs2340a_team19.R;
+public class IngredientFeaturesFragment extends Fragment {
+    private EditText textIngredientName;
+    private EditText textQuantity;
+    private EditText textCalories;
+    private EditText textExpirationDate;
+    private Button addIngredient;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_ingredient_features, container, false);
+        textIngredientName = root.findViewById(R.id.textIngredientName);
+        textQuantity = root.findViewById(R.id.textQuantity);
+        textCalories = root.findViewById(R.id.textCalories);
+        textExpirationDate = root.findViewById(R.id.textExpirationDate);
+        addIngredient = root.findViewById(R.id.addIngredient);
+        addIngredient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ingredientNameStr = textIngredientName.getText().toString();
+                String quantityStr = textQuantity.getText().toString();
+                String caloriesStr = textCalories.getText().toString();
+                String expirationDateStr = textExpirationDate.getText().toString();
+
+                if (ingredientNameStr.isEmpty() || quantityStr.isEmpty() || caloriesStr.isEmpty()) {
+                    Toast.makeText(getContext(), "Please enter something", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                int quantity = Integer.parseInt(quantityStr);
+                double calories = Double.parseDouble(caloriesStr);
+                if (quantity <= 0 || calories <= 0) {
+                    Toast.makeText(getContext(), "Negative values are not possible", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(getContext(), "Ingredient added", Toast.LENGTH_SHORT).show();
+                textIngredientName.setText("");
+                textQuantity.setText("");
+                textCalories.setText("");
+                textExpirationDate.setText("");
+            }
+        });
+        return root;
+    }
+}
+
--- app/src/main/java/com/example/cs2340a_team19/ui/ingredients/IngredientsFragment.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/ingredients/IngredientsFragment.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.example.cs2340a_team19.R;
 import com.example.cs2340a_team19.databinding.FragmentIngredientsBinding;
 
 public class IngredientsFragment extends Fragment {
 
     private FragmentIngredientsBinding binding;
+    private Button formButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,14 +30,32 @@ public class IngredientsFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textIngredients;
+        formButton = root.findViewById(R.id.formButton);
+//        formButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                openFormEntryDialog();
+//            }
+//        });
 
+
+        formButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_ingredients_to_ingredientFeaturesFragment);
+            }
+
+        });
         ingredientsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    /*private void openFormEntryDialog() {
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FormEntryDialogFragment formEntryDialog = new FormEntryDialogFragment();
+        formEntryDialog.show(fragmentManager, "FormEntryDialog");
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }*/
     }
 }
