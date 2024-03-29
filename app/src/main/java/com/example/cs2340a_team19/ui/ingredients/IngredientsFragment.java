b5c8193e5b20455ebc8f9995df25f83679c0b219
sidchill <siddharth.chilluvuri@gmail.com>
2024-03-29 19:40:10 -0400

    Created LoginViewModel JUnit Test, that tests all the possible invalid and valid password and email sequences

--- app/src/main/java/com/example/cs2340a_team19/ui/ingredients/IngredientsFragment.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/ingredients/IngredientsFragment.java
@@ -4,18 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.example.cs2340a_team19.R;
 import com.example.cs2340a_team19.databinding.FragmentIngredientsBinding;
-import com.example.cs2340a_team19.ui.recipe.RecipeViewModel;
 
 public class IngredientsFragment extends Fragment {
 
     private FragmentIngredientsBinding binding;
+    private Button formButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +29,16 @@ public class IngredientsFragment extends Fragment {
         binding = FragmentIngredientsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        final TextView textView = binding.textIngredients;
+        formButton = root.findViewById(R.id.formButton);
+        formButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_ingredients_to_ingredientFeaturesFragment);
+            }
+
+        });
+        ingredientsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
