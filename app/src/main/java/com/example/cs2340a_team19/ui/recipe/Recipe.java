1fd6f53e60c6c11cec4c15b7e59feb2d8765768e
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-03-30 21:31:14 -0400

    bodged fixes regarding Recipe
    TODO: integeration with database handlers that are absolutely not going too well rn

--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/Recipe.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/Recipe.java
@@ -1,8 +1,13 @@
 package com.example.cs2340a_team19.ui.recipe;
-
+import com.example.cs2340a_team19.ui.ingredients.Ingredient;
+import java.util.List;
 public class Recipe {
 
     public Recipe(String name, int calories, List<Ingredient> ingre) {
 
+    }
+
+    public String getName(){
+        return "tempName";
     }
 }
--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/recipeSorter.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/recipeSorter.java
@@ -3,5 +3,5 @@ package com.example.cs2340a_team19.ui.recipe;
 import java.util.ArrayList;
 
 public interface recipeSorter {
-    public static ArrayList<String> sortRecipes(ArrayList<Recipe> recipes);
+    //public static ArrayList<String> sortRecipes(ArrayList<Recipe> recipes);
 }
