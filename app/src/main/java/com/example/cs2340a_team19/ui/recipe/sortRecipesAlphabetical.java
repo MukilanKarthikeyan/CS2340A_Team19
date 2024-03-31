03b5fd0059ecf71d977b076b5c915648f9571e23
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-03-31 10:56:43 -0400

    Updated recipe sorting to return list of recipes instead of just the names for easy of traversal with display

--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/sortRecipesAlphabetical.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/sortRecipesAlphabetical.java
@@ -2,19 +2,12 @@ package com.example.cs2340a_team19.ui.recipe;
 import com.example.cs2340a_team19.models.Recipe;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 
 public class sortRecipesAlphabetical implements recipeSorter{
-    public ArrayList<String> sortRecipes(ArrayList<Recipe> recipes) {
-        ArrayList<String> recipeNames = new ArrayList<>();
-
-        for (Recipe curr : recipes) {
-            String currName = curr.getName();
-            recipeNames.add(currName);
-        }
-
-        Collections.sort(recipeNames);
-
-        return recipeNames;
+    public ArrayList<Recipe> sortRecipes(ArrayList<Recipe> recipes) {
+        ArrayList<Recipe> sortedRecipe = new ArrayList<>(recipes);
+        sortedRecipe.sort(Comparator.comparing(Recipe::getName));
+        return sortedRecipe;
     }
 }
--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/sortRecipesReverseAlphabetical.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/sortRecipesReverseAlphabetical.java
@@ -5,18 +5,10 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class sortRecipesReverseAlphabetical implements recipeSorter{
-    public ArrayList<String> sortRecipes(ArrayList<Recipe> recipes) {
-        ArrayList<String> recipeNames = new ArrayList<>();
-
-        for (Recipe curr : recipes) {
-            String currName = curr.getName();
-            recipeNames.add(currName);
-        }
-
-        Collections.sort(recipeNames);
-
-        Collections.reverse(recipeNames);
-
-        return recipeNames;
+    public ArrayList<Recipe> sortRecipes(ArrayList<Recipe> recipes) {
+        ArrayList<Recipe> sortedRecipe = new ArrayList<>(recipes);
+        sortedRecipe.sort(Comparator.comparing(Recipe::getName));
+        Collections.reverse(sortedRecipe);
+        return sortedRecipe;
     }
 }
