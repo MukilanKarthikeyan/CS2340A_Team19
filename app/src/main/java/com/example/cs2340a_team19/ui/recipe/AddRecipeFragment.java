45fdbba44e514cc96fdfbb002a426a550cb819cd
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-03-31 21:50:07 -0400

    3b UI congrucency finnaly fixed added a button that takes you to a new page to input the data for the form

--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/AddRecipeFragment.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/AddRecipeFragment.java
@@ -1,4 +1,4 @@
-package com.example.cs2340a_team19;
+package com.example.cs2340a_team19.ui.recipe;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.cs2340a_team19.R;
 
 /**
  * A simple {@link Fragment} subclass.
