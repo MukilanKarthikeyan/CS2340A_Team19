ae9dd74679877b2c861a9266766ae685fe4c6d18
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-04-01 13:23:24 -0400

    resizing recipe screen

--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/RecipeAdapter.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/RecipeAdapter.java
@@ -1,7 +1,5 @@
 package com.example.cs2340a_team19.ui.recipe;
 
-import static androidx.appcompat.graphics.drawable.DrawableContainerCompat.Api21Impl.getResources;
-
 import static java.security.AccessController.getContext;
 
 import android.annotation.SuppressLint;
@@ -13,11 +11,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
-<<<<<<< HEAD
 import androidx.recyclerview.widget.LinearLayoutManager;
-=======
 import androidx.core.content.res.ResourcesCompat;
->>>>>>> fd2b99191fc0e72224f93ec1740583c56012a457
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cs2340a_team19.R;
@@ -33,22 +28,20 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
     private List<Recipe> recipeList;
     class RecipeViewHolder extends RecyclerView.ViewHolder {
         public TextView recipeNameLabel;
-<<<<<<< HEAD
         public RecyclerView ingredientsList;
-=======
+
         public CardView recipePantryStatus;
->>>>>>> fd2b99191fc0e72224f93ec1740583c56012a457
+
 
         public RecipeViewHolder(View view) {
             super(view);
             recipeNameLabel = view.findViewById(R.id.recipe_name);
-<<<<<<< HEAD
+
             ingredientsList = view.findViewById(R.id.recipe_ingredient_list);
 //            ingredientsList.setLayoutManager(new LinearLayoutManager(getActivity()));
-=======
+
             recipePantryStatus = view.findViewById(R.id.pantry_status_indicator);
 
->>>>>>> fd2b99191fc0e72224f93ec1740583c56012a457
             //quantitylabel = view.findViewById(R.id.ingredient_quantity);
 
         }
