30997cca80c22bcecfc7d8459613f0a59a5c3cad
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-03-31 21:55:56 -0400

    intermediate commit

--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/AddRecipeFragment.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/AddRecipeFragment.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.cs2340a_team19.R;
+import com.example.cs2340a_team19.ui.ingredients.IngredientsViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,7 +62,40 @@ public class AddRecipeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_recipe, container, false);
+        View root = inflater.inflate(R.layout.fragment_input_ingredient_form, container, false);
+        textIngredientName = root.findViewById(R.id.textIngredientName);
+        textQuantity = root.findViewById(R.id.textQuantity);
+        textCalories = root.findViewById(R.id.textCalories);
+        textExpirationDate = root.findViewById(R.id.textExpirationDate);
+        addIngredient = root.findViewById(R.id.addIngredient);
+        IngredientsViewModel ingredientsViewModel = new IngredientsViewModel();
+        addIngredient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ingredientNameStr = textIngredientName.getText().toString();
+                String quantityStr = textQuantity.getText().toString();
+                String caloriesStr = textCalories.getText().toString();
+                String expirationDateStr = textExpirationDate.getText().toString();
+
+                if (ingredientNameStr.isEmpty() || quantityStr.isEmpty() || caloriesStr.isEmpty()) {
+                    Toast.makeText(getContext(), "Please enter something", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                int quantity = Integer.parseInt(quantityStr);
+                double calories = Double.parseDouble(caloriesStr);
+                if (quantity <= 0 || calories <= 0) {
+                    Toast.makeText(getContext(), "Negative values are not possible", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                ingredientsViewModel.addIngredient(ingredientNameStr, quantity, (int) calories, expirationDateStr);
+                Toast.makeText(getContext(), "Ingredient added", Toast.LENGTH_SHORT).show();
+                textIngredientName.setText("");
+                textQuantity.setText("");
+                textCalories.setText("");
+                textExpirationDate.setText("");
+            }
+        });
+        return root;
     }
 }
