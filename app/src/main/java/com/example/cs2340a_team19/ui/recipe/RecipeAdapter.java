5b3ee9ef97d20de7464b590c95563c3158218cd5
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-03-31 23:20:36 -0400

    recipe adapter

--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/RecipeAdapter.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/RecipeAdapter.java
@@ -1,56 +1,80 @@
 package com.example.cs2340a_team19.ui.recipe;
-
+import android.annotation.SuppressLint;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cs2340a_team19.R;
+import com.example.cs2340a_team19.models.DatabaseHandler;
+import com.example.cs2340a_team19.models.Ingredient;
+import com.example.cs2340a_team19.models.PantryHandler;
 import com.example.cs2340a_team19.models.Recipe;
+import com.example.cs2340a_team19.ui.ingredients.IngredientsAdapter;
 
 import java.util.List;
 
-public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+public class RecipeAdapter {
     private List<Recipe> recipeList;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView recipeNameTextView;
-        public TextView ingredientListTextView;
-        public TextView quantityListTextView;
+    private DatabaseHandler dbHandler;
+    private PantryHandler pantryHandler;
+    class RecipeViewHolder extends RecyclerView.ViewHolder {
+        public TextView recipeNameLabel;
 
-        public ViewHolder(View view) {
+
+        public RecipeViewHolder(View view) {
             super(view);
-            recipeNameTextView = view.findViewById(R.id.RecipeName);
-            ingredientListTextView = view.findViewById(R.id.IngredientList);
-            quantityListTextView = view.findViewById(R.id.QuantitiesList);
+            recipeNameLabel = view.findViewById(R.id.recipe_name);
         }
+
+    }
+    public RecipeAdapter(List<Recipe> itemList) {
+        this.recipeList = itemList;
     }
 
-    public RecipeAdapter(List<Recipe> recipeList) {
-        this.recipeList = recipeList;
-    }
-
-    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.recipe_list_item, parent, false);
-        return new ViewHolder(itemView);
+                .inflate(R.layout.ingredient_list_item, parent, false);
+
+        this.dbHandler = DatabaseHandler.getInstance();
+        this.pantryHandler = dbHandler.getPantryHandler();
+        if (dbHandler.isSuccessfullyInitialized() && dbHandler.getUserID() != null) {
+            // Add event listeners here!
+        } else {
+            Log.d("FBRTDB_ERROR", "Couldn't add Listener to Profile because dbHandler Initialization Failed!");
+        }
+        return new RecipeAdapter.RecipeViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Recipe recipe = recipeList.get(position);
-        holder.recipeNameTextView.setText(recipe.getName());
-        holder.ingredientListTextView.setText(recipe.getIngredients());
-        holder.quantityListTextView.setText(recipe.getQuantities());
+    public void onBindViewHolder(final RecipeAdapter.RecipeViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        final Recipe item = recipeList.get(position);
+        holder.recipeNameLabel.setText(item.name);
+
+//        holder.minusButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Decrease quantity if greater than 0
+//                if (item.quantity > 1) {
+//                    pantryHandler.updateIngredientQuantity(dbHandler.getUserID(), item.ingredientID, item.quantity - 1);
+//                    notifyItemChanged(position);
+//                } else {
+//                    pantryHandler.removeIngredient(dbHandler.getUserID(), item.ingredientID);
+//                }
+//            }
+//        });
     }
 
     @Override
     public int getItemCount() {
-        return recipeList.size();
+        return itemList.size();
     }
+
 }
