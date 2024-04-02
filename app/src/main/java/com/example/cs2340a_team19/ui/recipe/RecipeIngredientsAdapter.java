2b155af4adbb53916ecad345db2a817c5d97905d
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-04-01 22:23:46 -0400

    ingredient status indicator under recipie cards now accurately refelcts status of ability to make the recipe

--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/RecipeIngredientsAdapter.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/RecipeIngredientsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.cs2340a_team19.ui.recipe;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cs2340a_team19.R;
@@ -21,23 +24,28 @@ public class RecipeIngredientsAdapter extends RecyclerView.Adapter<RecipeIngredi
     private List<Ingredient> itemList;
     private List<Ingredient> pantry;
 
+    private Context context;
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView itemNameTextView;
         public TextView quantityAvailTextView;
         public TextView quantityNeededTextView;
 
+        public CardView ingredientStatus;
+
         public ViewHolder(View view) {
             super(view);
             itemNameTextView = view.findViewById(R.id.ingredientName);
             quantityAvailTextView = view.findViewById(R.id.ingredient_quantity_available);
-
             quantityNeededTextView = view.findViewById(R.id.ingredient_quantity_needed);
+            ingredientStatus = view.findViewById(R.id.recipe_ingredient_status);
         }
     }
 
-    public RecipeIngredientsAdapter(List<Ingredient> itemList, List<Ingredient> pantry) {
+    public RecipeIngredientsAdapter(List<Ingredient> itemList, List<Ingredient> pantry, Context context) {
         this.itemList = itemList;
         this.pantry = pantry;
+        this.context = context;
     }
 
     @Override
@@ -61,6 +69,10 @@ public class RecipeIngredientsAdapter extends RecyclerView.Adapter<RecipeIngredi
         holder.itemNameTextView.setText(item.name);
         holder.quantityAvailTextView.setText(String.valueOf(quantity));
         holder.quantityNeededTextView.setText(String.valueOf(item.quantity));
+
+
+        int recipeStatus = ContextCompat.getColor(this.context, (quantity >= item.quantity) ? R.color.green : R.color.red);
+        holder.ingredientStatus.setCardBackgroundColor(recipeStatus);
 
     }
 
