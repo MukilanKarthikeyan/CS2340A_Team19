c6a2bf92653a9068b90a5fd1300ca49abf617b35
alexichetov <alexichetov@gmail.com>
2024-04-02 14:45:43 -0400

    final checkstyle pass

--- app/src/main/java/com/example/cs2340a_team19/models/UserMeal.java
+++ app/src/main/java/com/example/cs2340a_team19/models/UserMeal.java
@@ -1,8 +1,8 @@
 package com.example.cs2340a_team19.models;
 
 public class UserMeal {
-    public String mealId;
-    public String date;
+    private String mealId;
+    private String date;
 
     public UserMeal() {
         this("", "");
@@ -10,6 +10,22 @@ public class UserMeal {
 
     public UserMeal(String mealId, String date) {
         this.mealId = mealId;
+        this.date = date;
+    }
+
+    public String getMealId() {
+        return mealId;
+    }
+
+    public void setMealId(String mealId) {
+        this.mealId = mealId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
         this.date = date;
     }
 }
