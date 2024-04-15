fca54390b9f89b6bf2f32ab2a5770b6e94b0a7f7
MukilanKarthikeyan <mukilankarthikeyan357@gmail.com>
2024-04-15 17:46:29 -0400

    checkstyle broke things i fixed things
    The checkstyle ran duplicated the viewholder inner class within RecipeIngredientsAdapter.java which grouped thing in a way that broke everything and because we were not informed of the issue and the PR was already approved we acciidently merged the things that broke everything

--- app/src/main/java/com/example/cs2340a_team19/ui/recipe/RecipeIngredientsAdapter.java
+++ app/src/main/java/com/example/cs2340a_team19/ui/recipe/RecipeIngredientsAdapter.java
@@ -21,51 +21,8 @@ public class RecipeIngredientsAdapter
         extends RecyclerView.Adapter<RecipeIngredientsAdapter.ViewHolder> {
     private List<Ingredient> itemList;
     private List<Ingredient> pantry;
-
     private Context context;
 
-    public RecipeIngredientsAdapter(List<Ingredient> itemList,
-                                    List<Ingredient> pantry, Context context) {
-        this.itemList = itemList;
-        this.pantry = pantry;
-        this.context = context;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.recipe_card_ingredient, parent, false);
-        return new ViewHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(final ViewHolder holder,
-                                 @SuppressLint("RecyclerView") final int position) {
-        final Ingredient item = itemList.get(position);
-        int quantity = 0;
-        for (Ingredient pantryIng : pantry) {
-            Log.d("GryphDebug", "Pantry Ing: " + (
-                    (pantryIng != null) ? pantryIng.getName() : "NULL"));
-            if (pantryIng.equals(item)) {
-                quantity = pantryIng.getQuantity();
-                break;
-            }
-        }
-        holder.itemNameTextView.setText(item.getName());
-        holder.quantityAvailTextView.setText(String.valueOf(quantity));
-        holder.quantityNeededTextView.setText(String.valueOf(item.getQuantity()));
-
-
-        int recipeStatus = ContextCompat.getColor(this.context, (
-                quantity >= item.getQuantity()) ? R.color.green : R.color.red);
-        holder.ingredientStatus.setCardBackgroundColor(recipeStatus);
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return itemList.size();
-    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView itemNameTextView;
@@ -115,4 +72,41 @@ public class RecipeIngredientsAdapter
         }
     }
 
+    public RecipeIngredientsAdapter(List<Ingredient> itemList, List<Ingredient> pantry, Context context) {
+        this.itemList = itemList;
+        this.pantry = pantry;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_card_ingredient, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder,
+                                 @SuppressLint("RecyclerView") final int position) {
+        final Ingredient item = itemList.get(position);
+        int quantityAvail = 0;
+        for (Ingredient pantryIng : pantry) {
+            if (pantryIng.equals(item)) {
+                quantityAvail = pantryIng.getQuantity();
+                break;
+            }
+        }
+        holder.itemNameTextView.setText(item.getName());
+        holder.quantityAvailTextView.setText(String.valueOf(quantityAvail));
+        holder.quantityNeededTextView.setText(String.valueOf(item.getQuantity()));
+
+        int status = ContextCompat.getColor(this.context, (quantityAvail >= item.getQuantity()) ? R.color.green : R.color.red);
+        holder.ingredientStatus.setCardBackgroundColor(status);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
 }
